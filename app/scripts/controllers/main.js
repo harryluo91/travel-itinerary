10c98797fa29058442461e2b911e64be47cbb7d3
Hao Luo <haoluo@Huas-MacBook-Pro.local>
2016-07-21 01:02:39 -0400

    question card update

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,20 @@
 angular.module('travelItineraryApp')
   .controller('MainCtrl', ['$scope', 'DAY_DETAILS', function($scope, DAY_DETAILS) {
     $scope.dayDetails = DAY_DETAILS;
+    $scope.masterCodeCorrect = false;
+    $scope.sectionVisible = [false, false, false, false, false, false, false, false, false, ];
     $scope.inputAnswers = [];
-    $scope.sectionVisible = [];
 
     $scope.checkAnswer = function(val, index) {
-      console.log(val);
-      if (val === '1') {
-        $scope.sectionVisible[index] = true;
+      if (index === 8 && (val === 'our3wishes')) {
+        $scope.sectionVisible.forEach(function(x, index, arr) {
+          arr[index] = true;
+        })
+        console.log($scope.sectionVisible)
+      } else if (index === 0 && (val === 'fir' || val === 'FIR')) {
+        $scope.sectionVisible[0] = true;
+      } else if (index === 1 && (val === 'fir' || val === 'FIR')) {
+        $scope.sectionVisible[1] = true;
       }
     }
   }]);
